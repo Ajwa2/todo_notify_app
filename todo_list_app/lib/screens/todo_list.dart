11e24ee253314ff4daf8c7c18bd6912840ce78fc
Sumeya <159885479+Ajwa2@users.noreply.github.com>
2024-02-13 15:59:32 +0300

    upload file

---
+++ todo_list_app/lib/screens/todo_list.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_app/screens/add_page.dart';
+import 'package:todo_list_app/services/todo_service.dart';
+import 'package:todo_list_app/utils/snackbar_helper.dart';
+import 'package:todo_list_app/widget/todo_card.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+        actions: [
+          IconButton(onPressed: navigateToAddPage, icon: Icon(Icons.add))
+        ],
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: Visibility(
+            visible: items.isNotEmpty,
+            replacement: Center(
+              child: Text(
+                'No Todo Item',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            child: ListView.builder(
+                itemCount: items.length,
+                padding: EdgeInsets.all(8),
+                itemBuilder: (context, index) {
+                  final item = items[index] as Map;
+                  return TodoCard(
+                      index: index,
+                      item: item,
+                      navigateEdit: navigateToEditPage,
+                      deleteById: deleteById);
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> navigateToEditPage(Map item) async {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(todo: item),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> navigateToAddPage() async {
+    final route = MaterialPageRoute(builder: (context) => const AddTodoPage());
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> deleteById(String id) async {
+    final isSuccess = await TodoService.deleteById(id);
+    if (isSuccess) {
+      //remove item from the list
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+      // fetchTodo();
+    } else {
+      showErrorMessage(context, message: 'deletion failed');
+    }
+  }
+
+  Future<void> fetchTodo() async {
+    final response = await TodoService.fetchTodos();
+    if (response != null) {
+      setState(() {
+        items = response;
+      });
+    } else {
+      showErrorMessage(context, message: 'something went wrong');
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+}
---
+++ todo_list_app/lib/widget/todo_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+
+class TodoCard extends StatelessWidget {
+  final int index;
+  final Map item;
+  final Function(Map) navigateEdit;
+  final Function(String) deleteById;
+  // final DateTime dateTime;
+  // final TimeOfDay timeOfDay;
+
+  const TodoCard({
+    super.key,
+    required this.index,
+    required this.item,
+    required this.navigateEdit,
+    required this.deleteById,
+    // required this.dateTime,
+    // required this.timeOfDay,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final id = item['_id'] as String;
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(child: Text('${index + 1}')),
+        title: Text(item['title']),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(item['description']),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Text(DateFormat.yMMMd().format(dateTime).toString()),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  // child: Text(timeOfDay.format(context).toString()),
+                )
+              ],
+            ),
+          ],
+        ),
+        trailing: PopupMenuButton(onSelected: (value) {
+          if (value == 'edit') {
+            //open the edit button
+            navigateEdit(item);
+
+          } else if (value == 'delete') {
+            //delete and remove the item
+            deleteById(id);
+          }
+        }, itemBuilder: (context) {
+          return const [
+            PopupMenuItem(
+              value: 'edit',
+              child: Text('Edit'),
+            ),
+            PopupMenuItem(
+              value: 'delete',
+              child: Text('Delete'),
+            )
+          ];
+        }),
+      ),
+    );
+  }
+}
